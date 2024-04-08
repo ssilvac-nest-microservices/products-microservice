6d3e3ac3e7280e2805f1b0a7501cefdfedf3b22d
Sebastián Silva <sebasilvac88@gmail.com>
2024-04-07 23:37:37 -0400

    implement nats

--- README.md
+++ README.md
@@ -6,4 +6,8 @@
 2. Instalar dependencias
 3. Crear un archivo `.env` basado en el archivo `env.template`
 4. Ejecutar migracion de prisma `npx prisma migrate dev`
-5. Ejecutar `pnpm start:dev`
+5. levantar servidor nats
+```
+docker run -d --name nats-main -p 4222:4222 -p 8222:8222 nats
+```
+6. Ejecutar `pnpm start:dev`
--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,16 +4,21 @@ import * as joi from 'joi';
 interface IEnvVars {
   PORT: number;
   DATABASE_URL: string;
+  NATS_SERVERS: string[];
 }
 
 const envVarsSchema = joi
   .object({
     PORT: joi.number().required(),
     DATABASE_URL: joi.string().required(),
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
   })
   .unknown(true);
 
-const { error, value } = envVarsSchema.validate(process.env);
+const { error, value } = envVarsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
@@ -24,4 +29,5 @@ const envVars: IEnvVars = value;
 export const envs = {
   port: envVars.PORT,
   databaseUrl: envVars.DATABASE_URL,
+  natsServers: envVars.NATS_SERVERS,
 };
